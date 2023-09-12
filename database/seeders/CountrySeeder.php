f42f724153df33a2c08d022558cb500454fa7735
= <=>
2023-09-12 17:04:27 +0300

    modify countries model

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -13,21 +13,30 @@ class CountrySeeder extends Seeder
      */
     public function run(): void
     {
-        Country::create(
-            [
-                'name' => [
-                    'ar' => 'المملكة العربية السعودية',
-                    'en' => 'kingdom of saudi arabia'
-                ],
-                'phone_code' => '966',
-                'code' => 'ksa',
-                'currency_name' => [
-                    'ar' => 'ريال سعودى',
-                    'en' => 'Saudi Riyal',
-                ],
-                'currency_symbol' => 'SAR',
-                'is_active' => 1
-            ]
-        );
+        // Get all from the JSON file
+        $JSON_countries = Country::allJSON();
+        foreach ($JSON_countries as $country) {
+            Country::firstOrCreate([
+                'capital'             => ((isset($country['capital'])) ? $country['capital'] : null),
+                'citizenship'         => ((isset($country['citizenship'])) ? $country['citizenship'] : null),
+                'country_code'        => ((isset($country['country_code'])) ? $country['country_code'] : null),
+                'currency'            => ((isset($country['currency'])) ? $country['currency'] : null),
+                'currency_code'       => ((isset($country['currency_code'])) ? $country['currency_code'] : null),
+                'currency_sub_unit'   => ((isset($country['currency_sub_unit'])) ? $country['currency_sub_unit'] : null),
+                'full_name'           => ((isset($country['full_name'])) ? $country['full_name'] : null),
+                'iso_3166_2'          => ((isset($country['iso_3166_2'])) ? $country['iso_3166_2'] : null),
+                'iso_3166_3'          => ((isset($country['iso_3166_3'])) ? $country['iso_3166_3'] : null),
+                'name'                => ((isset($country['name'])) ? $country['name'] : null),
+                'region_code'         => ((isset($country['region_code'])) ? $country['region_code'] : null),
+                'sub_region_code'     => ((isset($country['sub_region_code'])) ? $country['sub_region_code'] : null),
+                'eea'                 => ((isset($country['eea'])) ? $country['eea'] : false),
+                'phone_code'           => ((isset($country['phone_code'])) ? $country['phone_code'] : null),
+                'currency_symbol'     => ((isset($country['currency_symbol'])) ? $country['currency_symbol'] : null),
+                'currency_decimals'   => ((isset($country['currency_decimals'])) ? $country['currency_decimals'] : null),
+                'flag'                => ((isset($country['flag'])) ? $country['flag'] : null),
+                'minLength'           => ((isset($country['minLength'])) ? $country['minLength'] : 0),
+                'maxLength'           => ((isset($country['maxLength'])) ? $country['maxLength'] : 0),
+            ]);
+        }
     }
 }
